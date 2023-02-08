040bf766d558b95b697930aa483b2e7da38d18b7
Jad L <92789238+NoAmpersand@users.noreply.github.com>
2023-02-09 00:48:11 +0100

    Le READ du CRUD

--- src/Controller/ManagerController.php
+++ src/Controller/ManagerController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\FilmRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ManagerController extends AbstractController
 {
-    #[Route('/manager', name: 'app_manager')]
-    public function index(): Response
+    #[Route('/manager', name: 'app_manager', methods: ['GET'])]
+    public function index(FilmRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
+        $films = $paginator->paginate(
+            $repository -> findAll(),
+            $request->query->getInt('page', 1),
+            10
+        );
+
         return $this->render('manager/index.html.twig', [
-            'controller_name' => 'ManagerController',
+            'films' => $films
         ]);
     }
 }
