66b5f0459b830a496c20fa38260c238ba6ea2d73
Lilia Bouroubi <lilia.bouroubi@gmail.com>
2023-02-23 20:43:25 +0100

    added UsersFixtures

--- src/DataFixtures/UsersFixtures.php
+++ src/DataFixtures/UsersFixtures.php
@@ -2,7 +2,48 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 class UsersFixtures
 {
+    public function __construct(
+    private UserPasswordHasherInterface $passwordEncoder)
+{}
+    public function load(ObjectManager $manager): void
+    {
+        $admin = new User();
+        $admin->setFirstName('Nicolas')
+            ->setLastName('Desole')
+            ->setAddress('1 rue de Paris')
+            ->setTelephoneNumber('O666666666')
+            ->setEmail('admin@demo.fr')
+            ->setHighschool('HIV')
+            ->setRoles(['ROLE_ADMIN'])
+            ->setPassword($this->passwordEncoder->hashPassword($admin, 'admin')
+            );
+        $manager->persist($admin);
+
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 0; $i < 50; $i++){
+                $user = new User();
+                $user->setFirstName($faker->firstName())
+                    ->setLastName($faker->lastName())
+                    ->setAddress($faker->address())
+                    ->setTelephoneNumber($faker->numerify('0#########'))
+                    ->setEmail($faker->email)
+                    ->setHighschool($faker->citySuffix())
+                    ->setRoles(['ROLE_USER'])
+                    ->setPassword($this->passwordEncoder->hashPassword($user, 'secret')
+                    );
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
 
 }
